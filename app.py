19d6d3184908bdfb1de1d4de42b14bd0c673622d
pranav2305 <pranav2305@gmail.com>
2022-04-24 04:04:58 +0530

    Update some algos and add some styles

--- app.py
+++ app.py
@@ -28,15 +28,31 @@ def disk_scheduling():
 
 @app.route('/disk-scheduling/cscan', methods=['GET'])
 def ds_cscan():
-    return render_template('disk_scheduling/cscan.html')
+    return render_template('disk_scheduling/cscan.html', algo='CSCAN')
 
 @app.route('/disk-scheduling/fcfs', methods=['GET'])
 def ds_fcfs():
-    return render_template('disk_scheduling/fcfs.html')
+    return render_template('disk_scheduling/fcfs.html', algo='FCFS')
 
 @app.route('/disk-scheduling/scan', methods=['GET'])
 def ds_scan():
-    return render_template('disk_scheduling/scan.html')
+    return render_template('disk_scheduling/scan.html', algo='SCAN')
+
+@app.route('/disk-scheduling/look', methods=['GET'])
+def ds_look():
+    return render_template('disk_scheduling/look.html', algo='LOOK')
+
+@app.route('/disk-scheduling/clook', methods=['GET'])
+def ds_clook():
+    return render_template('disk_scheduling/clook.html', algo='CLOOK')
+
+@app.route('/disk-scheduling/sstf', methods=['GET'])
+def ds_sstf():
+    return render_template('disk_scheduling/sstf.html', algo='SSTF')
+
+@app.route('/disk-scheduling/compare', methods=['GET'])
+def ds_compare():
+    return render_template('disk_scheduling/compare.html', algo='SSTF')
 
 @app.route('/file-allocation', methods=['GET'])
 def file_allocation():
@@ -44,15 +60,15 @@ def file_allocation():
 
 @app.route('/file-allocation/indexed', methods=['GET'])
 def fa_indexed():
-    return render_template('file_allocation/indexed.html')
+    return render_template('file_allocation/indexed.html', algo='Indexed')
 
 @app.route('/file-allocation/linked', methods=['GET'])
 def fa_linked():
-    return render_template('file_allocation/linked.html')
+    return render_template('file_allocation/linked.html', algo='Linked')
 
 @app.route('/file-allocation/sequential', methods=['GET'])
 def fa_sequential():
-    return render_template('file_allocation/sequential.html')
+    return render_template('file_allocation/sequential.html', algo='Sequential')
 
 @app.route('/memory-allocation', methods=['GET'])
 def memory_allocation():
@@ -74,21 +90,21 @@ def mft_first_fit():
 def mft_worst_fit():
     return render_template('memory_allocation/mft/worst_fit.html', algo='Worst Fit')
 
-@app.route('/memory-allocation/mvt', methods=['GET'])
+@app.route('/memory-allocation/mvt/', methods=['GET'])
 def memory_allocation_mvt():
     return render_template('memory_allocation/mvt/wiki.html')
 
 @app.route('/memory-allocation/mvt/best-fit', methods=['GET'])
 def mvt_best_fit():
-    return render_template('memory_allocation/mvt/best_fit.html')
+    return render_template('memory_allocation/mvt/best_fit.html', algo='Best Fit')
 
 @app.route('/memory-allocation/mvt/first-fit', methods=['GET'])
 def mvt_first_fit():
-    return render_template('memory_allocation/mvt/first_fit.html')
+    return render_template('memory_allocation/mvt/first_fit.html', algo='First Fit')
 
 @app.route('/memory-allocation/mvt/worst-fit', methods=['GET'])
 def mvt_worst_fit():
-    return render_template('memory_allocation/mvt/worst_fit.html')
+    return render_template('memory_allocation/mvt/worst_fit.html', algo='Worst Fit')
 
 @app.route('/page-replacement', methods=['GET'])
 def page_replacement():
@@ -106,17 +122,17 @@ def pa_lru():
 def pa_optimal():
     return render_template('page_replacement/optimal.html')
 
-@app.route('/paging', methods=['GET'])
-def paging():
-    return render_template('paging/wiki.html')
+# @app.route('/paging', methods=['GET'])
+# def paging():
+#     return render_template('paging/wiki.html')
 
-@app.route('/paging/single-level', methods=['GET'])
-def p_single_level():
-    return render_template('paging/single_level.html')
+# @app.route('/paging/single-level', methods=['GET'])
+# def p_single_level():
+#     return render_template('paging/single_level.html')
 
-@app.route('/paging/multi-level', methods=['GET'])
-def p_multi_level():
-    return render_template('paging/multi_level.html')
+# @app.route('/paging/multi-level', methods=['GET'])
+# def p_multi_level():
+#     return render_template('paging/multi_level.html')
 
 @app.route('/process-scheduling', methods=['GET'])
 def process_scheduling():
@@ -138,9 +154,17 @@ def ps_round_robin():
 def ps_sjf():
   return render_template('process_scheduling/sjf.html', algo='SJF')
 
+@app.route('/process-scheduling/ljf', methods=['GET'])
+def ps_ljf():
+  return render_template('process_scheduling/ljf.html', algo='LJF')
+
 @app.route('/process-scheduling/srtf', methods=['GET'])
 def ps_srtf():
   return render_template('process_scheduling/srtf.html', algo='SRTF')
+
+@app.route('/process-scheduling/hrrn', methods=['GET'])
+def ps_hrrn():
+  return render_template('process_scheduling/hrrn.html', algo='HRRN')
   
 @app.route('/process-scheduling/compare', methods=['GET'])
 def ps_compare():
@@ -161,6 +185,10 @@ def ps_reader_writer():
 @app.route('/process-sync/producer-consumer', methods=['GET'])
 def ps_producer_consumer():
     return render_template('process_sync/producer_consumer.html')
+
+@app.route('/process-sync/sleeping-barber', methods=['GET'])
+def ps_sleeping_barber():
+    return render_template('process_sync/sleeping_barber.html')
 
 
 if __name__ == '__main__':
