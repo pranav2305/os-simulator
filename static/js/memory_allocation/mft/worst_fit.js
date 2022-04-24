11cbf3fcfed9ab0e74ba178851de3d27695e6585
pranav2305 <pranav2305@gmail.com>
2022-04-25 03:13:16 +0530

    Add styles for mft

--- static/js/memory_allocation/mft/worst_fit.js
+++ static/js/memory_allocation/mft/worst_fit.js
@@ -27,17 +27,20 @@ function displayPartSize() {
     for (i = 1; i <= num_parts; i++) {
         htmlText +=
             `
-        <div class="form-group">
-            <label>Size of partition ` + String(i) + ` : </label>
-            <input type="text" class="form-control" id="part-size-` + String(i) + `" placeholder="Enter size of partitition ` + String(i) + `">
+        <div class="input-div">
+            <label>Size of partition ` + String(i) + `: </label>
+            <input type="text" id="part-size-` + String(i) + `" placeholder="Enter size of partitition ` + String(i) + `">
         </div>
         `;
     }
     htmlText +=
         `
-    <button type="submit" class="btn btn-primary" id="parts-size-btn">Go</button>
+    <div class="button-div">
+        <button type="submit" id="parts-size-btn">Go</button>
+    </div>
     `;
     $("#parts-size-form").html(htmlText);
+    document.getElementById("parts-size-form").style.display = "flex";
     $(document).ready(function () {
         $("#parts-size-btn").click(function () {
             startColumn2();
@@ -48,8 +51,10 @@ function displayPartSize() {
 function startColumn2() {
     var htmlText =
         `
-    <button type="submit" class="btn btn-primary" id="add-pro-btn">Add process</button>
-    <button type="submit" class="btn btn-primary" id="rem-pro-btn">Remove process</button>
+    <div class="button-div" style="justify-content: center;">
+        <button type="submit" id="add-pro-btn">Add process</button>
+        <button type="submit" id="rem-pro-btn">Remove process</button>
+    </div>
     `;
     $("#add-rem-pro-btns").html(htmlText);
     var htmlText =
@@ -67,17 +72,20 @@ function startColumn2() {
             remProcessId();
         });
     });
-
+    document.getElementById("add-rem-pro-container").style.display = "flex";
+    document.getElementById("canvas-container").style.display = "flex";
 }
 
 function addProcessSize() {
     var htmlText =
         `
-    <div class="form-group">
+    <div class="input-div">
         <label>Size of process to be added: </label>
-        <input type="text" class="form-control" id="add-pro-size" placeholder="Enter size of process to be added">      
+        <input type="text" id="add-pro-size" placeholder="Enter size of process to be added">      
     </div>
-    <button type="submit" class="btn btn-primary" id="add-btn">Add</button>
+    <div class="button-div">
+        <button type="submit" id="add-btn">Add</button>
+    </div>
     `;
     $("#add-rem-pro").html(htmlText);
     $(document).ready(function () {
@@ -131,7 +139,7 @@ function drawPart(pro_size, pro_id, index) {
 
     ctx.beginPath();
     ctx.rect(myCanvas_x_start, part_myCanvas_start[index], myCanvas_width, pro_size * (500 / total_mem_size));
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "#9cff3f";
     ctx.fill();
 
     ctx.font = "14px Arial bold";
@@ -142,11 +150,13 @@ function drawPart(pro_size, pro_id, index) {
 function remProcessId() {
     var htmlText =
         `
-    <div class="form-group">
+    <div class="input-div">
         <label>Id of process to be removed: </label>
-        <input type="text" class="form-control" id="rem-pro-id" placeholder="Enter id of process to be removed">      
+        <input type="text" id="rem-pro-id" placeholder="Enter id of process to be removed">      
     </div>
-    <button type="submit" class="btn btn-primary" id="rem-btn">Remove</button>
+    <div class="button-div">
+        <button type="submit" id="rem-btn">Remove</button>
+    </div>
     `;
     $("#add-rem-pro").html(htmlText);
     $(document).ready(function () {
@@ -169,7 +179,7 @@ function remProcess(id_pro) {
             var ctx = document.getElementById("myCanvas").getContext("2d");
             ctx.beginPath();
             ctx.rect(myCanvas_x_start, part_myCanvas_start[i], myCanvas_width, part_size[i] * (500 / total_mem_size));
-            ctx.fillStyle = "white";
+            ctx.fillStyle = "rgb(31, 40, 51)";
             ctx.fill();
 
             ctx.rect(myCanvas_x_start, part_myCanvas_start[i], myCanvas_width, part_size[i] * (500 / total_mem_size))
@@ -239,36 +249,28 @@ function removeFromQ(pro_id) {
 function drawInputQTable() {
     var htmlText =
         `
-    <table>
-    <tr>
-        <th colspan="0">Input Queue</th>
-    </tr>
+    <table class="table table-bordered">
     <tr>
         <th>Process Id</th>
-    `;
-    for (var i = 0; i < input_q_size; i++) {
-        htmlText +=
-            `
-        <td>` + input_q_pro_id[i] + `</td>
-        `;
-    }
-
-    htmlText +=
-        `
-    <tr>
         <th>Process Size</th>
+    </tr>
     `;
+   
     for (var i = 0; i < input_q_size; i++) {
         htmlText +=
             `
-        <td>` + input_q_pro_size[i] + `</td>
+        <tr>
+            <td>` + input_q_pro_id[i] + `</td>
+            <td>` + input_q_pro_size[i] + `</td>
+        </tr>
         `;
     }
 
+
     htmlText +=
         `
-    </tr>
     </table>
     `;
     $("#input-q-table").html(htmlText);
+    document.getElementById('q-table-container').style.display = 'flex';
 }
