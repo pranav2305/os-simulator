66522d650b95fc0bc329257cdae5586d3e755836
Krilufy <rahulsyam920@gmail.com>
2022-04-26 03:06:29 +0530

    ch5

--- static/js/process_sync/reader_writer.js
+++ static/js/process_sync/reader_writer.js
@@ -15,11 +15,11 @@ $(document).ready(function () {
             alert("Cannot write while document is being read!");
     });
 
-    $('#startread1').click(function reader1() {
+    function reader(r){
         if (flag == 1 || count > 0) {
-            document.getElementById('stopread1').style.visibility = 'visible';
-            document.getElementById('startread1').style.visibility = 'hidden';
-            document.getElementById('textarea1').style.visibility = 'visible';
+            document.getElementById(`stopread${r}`).style.visibility = 'visible';
+            document.getElementById(`startread${r}`).style.visibility = 'hidden';
+            document.getElementById(`textarea${r}`).style.visibility = 'visible';
             var n1 = document.getElementById('textarea');
             count++;
             
@@ -29,151 +29,54 @@ $(document).ready(function () {
             }
             
             document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea1').value = '';
-            var n2 = document.getElementById('textarea1');
+            document.getElementById(`textarea${r}`).value = '';
+            var n2 = document.getElementById(`textarea${r}`);
             n2.value = n1.value;
             
         }
         else 
             alert("Cannot read while document is being modified!");   
-    });
-    $('#startread2').click(function reader2() {
-        if (flag == 1 || count > 0) {
-            document.getElementById('stopread2').style.visibility = 'visible';
-            document.getElementById('startread2').style.visibility = 'hidden';
-            document.getElementById('textarea2').style.visibility = 'visible';
-            var n1 = document.getElementById('textarea');
-            count++;
-            //var n = flag % 5;
-            //console.log("reader", flag);
-            if (count == 1){
-                flag = 0;
-                document.getElementById("textarea").disabled = true;
-            }
-            
-            document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea2').value = '';
-            var n2 = document.getElementById('textarea2');
-            n2.value = n1.value;
-            
-        }
-        else 
-            alert("Cannot read while document is being modified!");   
-    });
-    $('#startread3').click(function reader3() {
-        if (flag == 1 || count > 0) {
-            document.getElementById('stopread3').style.visibility = 'visible';
-            document.getElementById('startread3').style.visibility = 'hidden';
-            document.getElementById('textarea3').style.visibility = 'visible';
-            var n1 = document.getElementById('textarea');
-            count++;
-            //var n = flag % 5;
-            //console.log("reader", flag);
-            if (count == 1){
-                flag = 0;
-                document.getElementById("textarea").disabled = true;
-            }
-            
-            document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea3').value = '';
-            var n2 = document.getElementById('textarea3');
-            n2.value = n1.value;
-            
-        }
-        
-        else 
-            alert("Cannot read while document is being modified!");   
-    });
-    $('#startread4').click(function reader4() {
-        if (flag == 1 || count > 0) {
-            document.getElementById('stopread4').style.visibility = 'visible';
-            document.getElementById('startread4').style.visibility = 'hidden';
-            document.getElementById('textarea4').style.visibility = 'visible';
-            var n1 = document.getElementById('textarea');
-            count++;
-            //var n = flag % 5;
-            //console.log("reader", flag);
-            if (count == 1){
-                flag = 0;
-                document.getElementById("textarea").disabled = true;
-            }
-            
-            document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea4').value = '';
-            var n2 = document.getElementById('textarea4');
-            n2.value = n1.value;
-            
-        }
-        
-        else 
-            alert("Cannot read while document is being modified!");   
-    });
+    }
 
-    $('#stopread1').click(function stopread1() {
-            var n1 = document.getElementById('textarea');
-            count--;
-            //var n = flag % 5;
-            //console.log("reader", flag);
-            if (count == 0){
-                flag = 1;
-                document.getElementById("textarea").disabled = false;
-            }
-            document.getElementById('startread1').style.visibility = 'visible';
-            document.getElementById('stopread1').style.visibility = 'hidden';
-            document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea1').style.visibility = 'hidden';
-           
+    $('#startread1').click(function() {
+        reader(1);
     });
-    $('#stopread2').click(function stopread2() {
-        
-            var n1 = document.getElementById('textarea');
-            count--;
-            //var n = flag % 5;
-            //console.log("reader", flag);
-            if (count == 0){
-                flag = 1;
-                document.getElementById("textarea").disabled = false;
-            }
-            document.getElementById('startread2').style.visibility = 'visible';
-            document.getElementById('stopread2').style.visibility = 'hidden';
-            document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea2').style.visibility = 'hidden';
-        
-        
-               
+    $('#startread2').click(function() {
+        reader(2);
     });
-    $('#stopread3').click(function stopread3() {
-        
-            var n1 = document.getElementById('textarea');
-            count--;
-            //var n = flag % 5;
-            //console.log("reader", flag);
-            if (count == 0){
-                flag = 1;
-                document.getElementById("textarea").disabled = false;
-            }
-            document.getElementById('startread3').style.visibility = 'visible';
-            document.getElementById('stopread3').style.visibility = 'hidden';
-            document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea3').style.visibility = 'hidden';
-         
-               
+    $('#startread3').click(function() {
+        reader(3);
     });
-    $('#stopread4').click(function stopread4() {
-        
+    $('#startread4').click(function() {
+        reader(4);
+    });
+    
+
+function stopread(r) {
             var n1 = document.getElementById('textarea');
             count--;
             //var n = flag % 5;
             //console.log("reader", flag);
             if (count == 0){
                 flag = 1;
-                document.getElementById("textarea").disabled = false;
             }
-            document.getElementById('startread4').style.visibility = 'visible';
-            document.getElementById('stopread4').style.visibility = 'hidden';
+            document.getElementById(`startread${r}`).style.visibility = 'visible';
+            document.getElementById(`stopread${r}`).style.visibility = 'hidden';
             document.getElementById('textarea').style.background = "#E0E0E0 ";
-            document.getElementById('textarea4').style.visibility = 'hidden';
-           
+            document.getElementById(`textarea${r}`).style.visibility = 'hidden';
+    }
+
+    $('#stopread1').click(function()  {  
+        stopread(1);     
+    });
+    $('#stopread2').click(function()  {  
+        stopread(2);     
+    });
+    $('#stopread3').click(function()  {  
+        stopread(3);     
+    });
+    $('#stopread4').click(function()  {  
+        stopread(4);     
     });
 
     $('#stop').click(function stop() {
@@ -181,5 +84,6 @@ $(document).ready(function () {
         document.getElementById("writer").style.visibility = 'visible';
         document.getElementById("stop").style.visibility = 'hidden';
         document.getElementById("textarea").disabled = true;
+        document.getElementById('textarea').style.background = "#E0E0E0 ";
     });
 });
